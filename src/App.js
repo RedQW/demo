63389f98ab95032ddd2765309df07e5dc6a7fc29
RedQW <danhtang226@gmail.com>
2023-07-16 11:41:39 +0700

    first commissasdadt

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App
-        " alt="logo" />
+        <img
+          src={logo}
+          className="App
+        -logo"
+          alt="logo"
+        />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
